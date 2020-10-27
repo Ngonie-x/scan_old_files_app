fe2c5007abf3c900cf4457e3b98fa3c36ae0fe75
Ngonie-x <ngonidzashedelight@gmail.com>
2020-10-27 11:32:18 +0200

    Main project files

---
+++ detect.py
@@ -0,0 +1,49 @@
+import os
+from datetime import datetime, date
+    
+
+def scan_date(timestamp, date_obj):
+    x = datetime(year=date_obj.year, month=date_obj.month, day=date_obj.day)
+    d = datetime.utcfromtimestamp(timestamp)
+    if d < x:
+        return True
+
+
+def convert_date(timestamp):
+    d = datetime.utcfromtimestamp(timestamp)
+    formatted_date = d.strftime('%d %m %Y')
+
+    return formatted_date
+
+
+def update_trv(rows, trv):
+    trv.delete(*trv.get_children())
+    for i in rows:
+        trv.insert('', 'end', values=i)
+
+
+
+def simple_scan(filepath, date_obj, trv):
+    trv.delete(*trv.get_children())
+    for entry in os.scandir(filepath):
+        if entry.is_dir():
+            # print(entry.path)
+            path = str(entry.path)
+            new_path = path.replace('\\', '/')
+            simple_scan(entry, date_obj, trv)
+        else:
+            info =  entry.stat()
+            mod_date = info.st_mtime
+            if scan_date(info.st_mtime, date_obj):
+                file_name = entry.name
+                filesize = info.st_size
+                # print(entry.path + " : "+ convert_date(mod_date))
+                details = (file_name, filesize, convert_date(mod_date), entry.path)
+                trv.insert('', 'end', values=details)
+                
+
+
+
+
+
+
---
+++ mainapp.py
@@ -0,0 +1,83 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+from tkcalendar import Calendar, DateEntry
+from tkinter import filedialog as fd
+from send2trash import send2trash
+
+from detect import simple_scan
+
+
+class Main(Tk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("850x550")
+        self.title("Old Files Scanner")
+
+
+        self.date_frame = Frame(self)
+        self.date_frame.grid(row=0, column=0)
+
+        self.date_lbl = ttk.Label(self.date_frame, text="Scan for files before: ")
+        self.calendar = DateEntry(self.date_frame, width=12, background='darkblue', foreground='white', borderwidth=2, year=2020)
+        self.select_folder_btn = ttk.Button(self.date_frame, text="Select Folder", command=self.open_folder)
+        self.date_lbl.pack(side=LEFT, padx=5, pady=3)
+        self.calendar.pack(side=LEFT, padx=5, pady=3)
+        self.select_folder_btn.pack(side=LEFT, padx=5, pady=3)
+
+        self.status = StringVar()
+        self.status_lbl = ttk.Label(self, text = '', textvariable=self.status)
+        self.status_lbl.grid(row=1, column=0)
+
+
+        # Treeview wrapper
+        tree_wrapper = LabelFrame(self, text="Document List")
+        tree_wrapper.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+
+        self.trv= ttk.Treeview(tree_wrapper, columns=(1, 2, 3, 4), show="headings", height=15)
+        self.trv.pack(padx=5, pady=5)
+
+        self.trv.heading(1, text= 'File Name')
+        self.trv.heading(2, text= 'Size')
+        self.trv.heading(3, text= 'Date')
+        self.trv.heading(4, text= 'Path')
+
+        self.trv.bind('<Button-1>', self.get_row)
+
+        self.delete_btn = ttk.Button(self, text="Delete", command=self.delete_file)
+        self.delete_btn.grid(row=3, column=0, padx=5, pady=5)
+
+        
+
+
+    def print_date(self):
+        print(self.calendar.get_date())
+
+    def open_folder(self):
+        name = fd.askdirectory()
+        print(name)
+        self.get_files(str(name), self.calendar.get_date())
+
+    def get_files(self, filepath, dateobj):
+        simple_scan(filepath, dateobj, self.trv)
+
+
+    def get_row(self, event):
+        self.selected_item = self.trv.selection()[0]
+        
+
+    def delete_file(self):
+        if self.selected_item:
+            # print(self.trv.item(self.selected_item)['values'][3])
+            path = str(self.trv.item(self.selected_item)['values'][3])
+            new_str = path.replace('/', '\\')
+            send2trash(new_str)
+            self.trv.delete(self.selected_item)
+        
+        
+
+
+
+if __name__ == '__main__':
+    app = Main()
+    app.mainloop()
